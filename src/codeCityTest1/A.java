b65624fd8ba3529a8cc247b9c2ddd86e564cd11f
Punsara96 <chathushthari@gmail.com>
2020-03-07 14:55:20 +0530

    add class A

---
+++ src/codeCityTest1/A.java
@@ -0,0 +1,11 @@
+package codeCityTest1;
+
+public class A {
+	void p() {
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+	}
+}
--- src/codeCityTest1/Test.java
+++ src/codeCityTest1/Test.java
@@ -1,7 +1,11 @@
 package codeCityTest1;
 
-public class Test {
+public class Test extends A{
 	public static void main(String[] args) {
+		int x=8;
+		int y=2;
 		System.out.println("1");
+		System.out.println("2");
+		new Test().p();
 	}
 }
