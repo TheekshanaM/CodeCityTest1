97ad05e366365e1bab8e45ca428838a28fed0424
Punsara96 <chathushthari@gmail.com>
2020-03-07 16:26:11 +0530

    change values in Add method

--- src/calculation/AddTwoNumbers.java
+++ src/calculation/AddTwoNumbers.java
@@ -5,6 +5,8 @@ public class AddTwoNumbers {
         
 	      int num1 = 5;
 	      int num2 = 15;
+	      num1 =3;
+	      num2=7;
 	      int sum = num1 + num2;
 
 	      System.out.println("Sum of these numbers: "+sum);
