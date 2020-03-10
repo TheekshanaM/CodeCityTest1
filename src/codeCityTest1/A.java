d7320b551838d2640968f8913f600fe6ee87f8f0
Punsara96 <chathushthari@gmail.com>
2020-03-10 19:16:19 +0530

    long method and long statement

--- src/codeCityTest1/A.java
+++ src/codeCityTest1/A.java
@@ -2,6 +2,100 @@ package codeCityTest1;
 
 public class A {
 	void p() {
+		String x= "The catch clause of a try catch block should not be empty. If the catch clause is empty then it is detected as a bad smell in coding.";
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+		
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+		
+		
+		
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+		
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+		
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
+		System.out.println("gfdvf");
+		System.out.println("fdbdf");
+		System.out.println("fgfd");
+		System.out.println("fgd");
+		System.out.println("fddf");
 		System.out.println("gfdvf");
 		System.out.println("fdbdf");
 		System.out.println("fgfd");
