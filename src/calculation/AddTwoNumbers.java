fb1faf69c44f3b77cbd4827ef17322e6e11c6888
Punsara96 <chathushthari@gmail.com>
2020-03-10 19:41:31 +0530

    long identifier long parameter

--- src/calculation/AddTwoNumbers.java
+++ src/calculation/AddTwoNumbers.java
@@ -3,8 +3,11 @@ package calculation;
 public class AddTwoNumbers {
 	static final int num1 = 5;
     static final int num2 = 15;
-	public void add() {
+	public void add(int a, int b, int c, int d, int e, int f ) {
+		int abcdefghijklmnopqrstuvwxyzaaaaaaaaaaaaaaaa = 3;
         int sum = num1 + num2;
-        System.out.println("Sum of these numbers: "+sum);
+        if(a>3 && b<6 && c>5 && d<2) {
+        	System.out.println("Sum of these numbers: "+sum);
+        }
 	}
 }
