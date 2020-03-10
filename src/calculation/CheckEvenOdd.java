291500514191f04658110b386b21d4a85e928643
Punsara96 <chathushthari@gmail.com>
2020-03-10 21:20:46 +0530

    empty catch and missing default

--- src/calculation/CheckEvenOdd.java
+++ src/calculation/CheckEvenOdd.java
@@ -10,12 +10,26 @@ public class CheckEvenOdd {
 	    //The input provided by user is stored in num
 	    Scanner input = new Scanner(System.in);
 	    num = input.nextInt();
+	    switch (num) {
+		case 1:
+			System.out.println("Entered number is even");
+			break;
+
+		case 2:
+			System.out.println("Entered number is even");
+			break;
+		}
 
 	    /* If number is divisible by 2 then it's an even number
 	     * else odd number*/
-	    if ( num % 2 == 0 )
-	        System.out.println("Entered number is even");
-	     else
-	        System.out.println("Entered number is odd");
+	    try {
+	    	if ( num % 2 == 0 )
+		        System.out.println("Entered number is even");
+		     else
+		        System.out.println("Entered number is odd");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	    
 	  } 
 }
